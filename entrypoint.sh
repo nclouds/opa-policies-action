387f2b61afefae2d6e83c8a78b815b091fb17be8
anupclouddevops <anup.cloud.devops@gmail.com>
2022-05-15 13:38:27 +1000

    Debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,10 +5,11 @@ set -o pipefail
 
 echo "Terraform Plan to evaulate $INPUT_TFPLAN_JSON"
 
-ls -ltr 
-
 # Copy the JSON to policies forlder
-cp $INPUT_TFPLAN_JSON policies/Infrastructure/tfplan.json
+cp $INPUT_TFPLAN_JSON /policies/Infrastructure/tfplan.json
+
+# List files inside policies directory
+ls -ltr /policies
 
 # Run Opa 
 make opa
