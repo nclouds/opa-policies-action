48eb951c847a37ea205d26337d5fadff189da141
anupclouddevops <anup.cloud.devops@gmail.com>
2022-05-15 20:00:09 +1000

    Update Dockerfile

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,12 @@ echo "Terraform Plan to evaulate $INPUT_TFPLAN_JSON"
 ls -ltr /policies
 
 # Copy the JSON to policies forlder
-cp $INPUT_TFPLAN_JSON /policies/Infrastructure/tfplan.json
+cp -R /policies/Infrastructure .
+
+ls -ltr
+cp $INPUT_TFPLAN_JSON policies/Infrastructure/tfplan.json
+
+ls -ltr
 
 # Run Opa 
 make opa
