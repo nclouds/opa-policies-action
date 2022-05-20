6ff1cdefbc1a9ff66e671ad3699105a045fe76f7
anupclouddevops <anup.cloud.devops@gmail.com>
2022-05-21 09:47:28 +1000

    Updated Netrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,13 +10,13 @@ __inputs="
     Path to Terraform Plan: $INPUT_TFPLAN_JSON_PATH
     Additonal Data or Configuration Files: $INPUT_DATA_JSON_PATHS
 "
+# Switch to Github Workspace
+pushd /github/workspace
+
 
 # List files inside workspace
-ls -ltr /github/workspace
+ls -ltr
+cat tfplan.json
 
-
-# Exit Nicely
-if [ $? != 0] ; then
-  echo "OPA Checks failed"
-  exit 1
-fi
+# Come out of the workspace
+popd
