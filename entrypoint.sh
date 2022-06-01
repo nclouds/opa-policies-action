ede6dc52ebe4e421c7f350fb752625b416f24991
Anup Gupta <anup.cloud.devops@gmail.com>
2022-06-01 12:53:50 +1000

    Enable OPA Debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,7 @@ __inputs="
     Policies Directory: $INPUT_POLICIES_DIR
     Path to Terraform Plan: $INPUT_TFPLAN_JSON
     Additonal Data or Configuration Files: $INPUT_DATA_FILES
+    OPA Debug Mode: $INPUT_DEBUG
 "
 # Switch to Github Workspace
 cd /github/workspace
